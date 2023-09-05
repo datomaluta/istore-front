be99651b162802dbd659830d4af6bea25ffadaaa
datomaluta <datomaluta@gmail.com>
2023-09-05 09:20:21 +0400

    add signup stuff

--- src/components/auth/signUp/SignUp.tsx
+++ src/components/auth/signUp/SignUp.tsx
@@ -10,6 +10,7 @@ import PasswordInput from "../../sharedComponents/inputs/passwordInput/PasswordI
 import { useMutation } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
 import Alert from "../../sharedComponents/alert/Alert";
+import LoadingSpinner from "../../sharedComponents/loadingSpinner/LoadingSpinner";
 
 const SignUp = ({
   closeSignUpOpenSignInHandler,
@@ -30,7 +31,6 @@ const SignUp = ({
   });
 
   const name = useWatch({ name: "name", control });
-  console.log(name);
 
   useEffect(() => {
     setBackErrorStatusCode(0);
@@ -38,9 +38,12 @@ const SignUp = ({
 
   const registerUserMutation = useMutation({
     mutationFn: registerUser,
-    onSuccess: (data) => {
-      setSignUpModalIsVisible(false);
+    onSuccess: () => {
       setSuccessMessage("მომხმარებელი წარმატებით დარეგისტრირდა");
+      setTimeout(() => {
+        setSignUpModalIsVisible(false);
+        setSuccessMessage("");
+      }, 3500);
     },
     onError: (error: customAxiosError) => {
       if (error.response) {
@@ -59,61 +62,62 @@ const SignUp = ({
   };
 
   return (
-    <ModalDiv setModalIsVisible={setSignUpModalIsVisible}>
-      <h1 className="border-b border-t-greyForBorder dark:border-greyforText pb-4 text-center text-2xl font-bold">
-        {t("sign_up")}
-      </h1>
+    <>
+      <Alert message={successMessage} />
+      <ModalDiv setModalIsVisible={setSignUpModalIsVisible}>
+        <h1 className="border-b border-t-greyForBorder dark:border-greyforText pb-4 text-center text-2xl font-bold">
+          {t("sign_up")}
+        </h1>
 
-      <Alert />
+        <form onSubmit={handleSubmit(submitHandler)} className=" mt-4">
+          <CustomInput
+            register={register}
+            name="name"
+            label={t("name")}
+            placeholder={t("enter_name")}
+            type="text"
+            backErrorStatusCode={backErrorStatusCode}
+            frontError={t(errors["name"]?.message || "")}
+          />
+          <CustomInput
+            name="email"
+            register={register}
+            label={t("email")}
+            placeholder={t("enter_email")}
+            type="email"
+            frontError={t(errors["email"]?.message || "")}
+          />
 
-      <form onSubmit={handleSubmit(submitHandler)} className=" mt-4">
-        <CustomInput
-          register={register}
-          name="name"
-          label={t("name")}
-          placeholder={t("enter_name")}
-          type="text"
-          backErrorStatusCode={backErrorStatusCode}
-          frontError={t(errors["name"]?.message || "")}
-        />
-        <CustomInput
-          name="email"
-          register={register}
-          label={t("email")}
-          placeholder={t("enter_email")}
-          type="email"
-          frontError={t(errors["email"]?.message || "")}
-        />
-
-        <PasswordInput
-          name="password"
-          register={register}
-          label={t("password")}
-          placeholder={t("enter_password")}
-          frontError={t(errors["password"]?.message || "")}
-        />
-        <PasswordInput
-          name="confirm_password"
-          register={register}
-          label={t("retype_password")}
-          placeholder={t("confirm_password")}
-          frontError={t(errors["confirm_password"]?.message || "")}
-        />
-        <div className="flex justify-between text-sm text-gray-500 ">
-          <button
-            className={`${i18n.resolvedLanguage === "ka" && "font-arial"} 
+          <PasswordInput
+            name="password"
+            register={register}
+            label={t("password")}
+            placeholder={t("enter_password")}
+            frontError={t(errors["password"]?.message || "")}
+          />
+          <PasswordInput
+            name="confirm_password"
+            register={register}
+            label={t("retype_password")}
+            placeholder={t("confirm_password")}
+            frontError={t(errors["confirm_password"]?.message || "")}
+          />
+          <div className="flex justify-between text-sm text-gray-500 ">
+            <button
+              className={`${i18n.resolvedLanguage === "ka" && "font-arial"} 
             }`}
-            type="button"
-            onClick={closeSignUpOpenSignInHandler}
-          >
-            {t("already_have_an_account")}
+              type="button"
+              onClick={closeSignUpOpenSignInHandler}
+            >
+              {t("already_have_an_account")}
+            </button>
+          </div>
+          <button className="bg-primary text-white w-full py-3 h-12 rounded font-bold mt-6 relative">
+            {registerUserMutation.isLoading ? <LoadingSpinner /> : t("sign_up")}
           </button>
-        </div>
-        <button className="bg-primary text-white w-full py-3 rounded font-bold mt-6">
-          {registerUserMutation.isLoading ? "Loading..." : t("sign_up")}
-        </button>
-      </form>
-    </ModalDiv>
+        </form>
+      </ModalDiv>
+    </>
   );
 };
 
--- src/components/sharedComponents/alert/Alert.tsx
+++ src/components/sharedComponents/alert/Alert.tsx
@@ -1,8 +1,20 @@
-const Alert = () => {
+import { motion } from "framer-motion";
+import { PropsType } from "./types";
+
+const Alert = ({ message }: PropsType) => {
   return (
-    <div className="fixed bottom-4 right-4 bg-red-500 p-4 z-[9999]">
-      Here is message
-    </div>
+    <>
+      {message && (
+        <motion.div
+          initial={{ opacity: 0, x: 500 }}
+          animate={{ opacity: 1, x: 0, transition: { duration: 0.2 } }}
+          exit={{ opacity: 0, x: 500, transition: { duration: 0.2 } }}
+          className="fixed bottom-6 text-sm right-6 bg-green-500 rounded p-4 z-[9999]"
+        >
+          {message}
+        </motion.div>
+      )}
+    </>
   );
 };
 
